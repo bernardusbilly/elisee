cdf5e5915b79fad3d1ce5d538267c3a8c681a1df
Billy Tjiptoning <8053504+bernardusbilly@users.noreply.github.com>
2021-05-11 01:33:58 +0700

    update website

---
+++ precache-manifest.3a2e657a4dc6193a7b8f8d0afdea87ad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc85a8ba078f5366ad11df00b5632c7",
+    "url": "/elisee/index.html"
+  },
+  {
+    "revision": "79f0ce20f1372e0f242f",
+    "url": "/elisee/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "5d694479276771849fbd",
+    "url": "/elisee/static/css/main.4e5702b4.chunk.css"
+  },
+  {
+    "revision": "79f0ce20f1372e0f242f",
+    "url": "/elisee/static/js/2.35287c77.chunk.js"
+  },
+  {
+    "revision": "5d694479276771849fbd",
+    "url": "/elisee/static/js/main.0f884301.chunk.js"
+  },
+  {
+    "revision": "5a973dacd028de8362ca",
+    "url": "/elisee/static/js/runtime~main.146f835b.js"
+  },
+  {
+    "revision": "c10f15f179ff812edfa4ab94cb5e7652",
+    "url": "/elisee/static/media/Brandon_blk.c10f15f1.otf"
+  },
+  {
+    "revision": "30045de3fb52345252898ee8b1b7c1a5",
+    "url": "/elisee/static/media/Brandon_light.30045de3.otf"
+  },
+  {
+    "revision": "a989c5b7849027ec20bcf8e716f2387f",
+    "url": "/elisee/static/media/Brandon_med.a989c5b7.otf"
+  },
+  {
+    "revision": "1f55925e52b9e71fd57108874184060d",
+    "url": "/elisee/static/media/Brandon_reg.1f55925e.otf"
+  },
+  {
+    "revision": "4be7a96e18ec1d5b1a0a4a212cefbef9",
+    "url": "/elisee/static/media/Brandon_thin.4be7a96e.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ea48f06413001f34a4df13396431b54.js"
+  "/elisee/precache-manifest.3a2e657a4dc6193a7b8f8d0afdea87ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/elisee/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
